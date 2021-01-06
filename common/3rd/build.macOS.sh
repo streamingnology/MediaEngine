0bc678492796a0d6ef4a526a8cbacef67b8baded
streamingnology <37998919+streamingnology@users.noreply.github.com>
2021-01-05 23:44:15 -0600

    Update build.macOS.sh

--- common/3rd/build.macOS.sh
+++ common/3rd/build.macOS.sh
@@ -5,86 +5,98 @@ build_path=$(pwd)/build
 
 build_Bento4(){
   echo "build Bento4 start"
-  Bento4_path_source=$source_path/Bento4
-  Bento4_path_build=$build_path/Bento4
-  mkdir -p $Bento4_path_build
-  cd $Bento4_path_source
+  library_source_path=$source_path/Bento4
+  library_build_path=$build_path/Bento4
+  mkdir -p $library_build_path
+  cd $library_source_path
   git checkout v1.5.1-629  
-  cd $Bento4_path_build
-  cmake -DCMAKE_BUILD_TYPE=Release $Bento4_path_source
-  make ap4/fast
+  cp -fr ../../patch/Bento4/CMakeLists.txt ./
+  cmake -DCMAKE_INSTALL_PREFIX=$library_build_path -DCMAKE_BUILD_TYPE=Release $library_source_path
+  make ap4
+  make ap4_a
+  make install
   echo "build Bento4 end"
 }
 
 build_gpac(){
   echo "build gpac start"
-  gpac_source_path=$source_path/gpac
-  gpac_build_path=$build_path/gpac
-  mkdir -p $gpac_build_path
-  cd $gpac_source_path
+  library_source_path=$source_path/gpac
+  library_build_path=$build_path/gpac
+  mkdir -p $library_build_path
+  cd $library_source_path
   git checkout v0.8.0
-  ./configure --prefix=$gpac_build_path --use-zlib=no && make && make install-lib
+  ./configure --prefix=$library_build_path --use-zlib=no && make && make install-lib
   echo "build gpac end"
 }
 
 build_openssl(){
   echo "build openssl start"
-  openssl_source_path=$source_path/openssl
-  openssl_build_path=$build_path/openssl
-  mkdir -p $openssl_build_path
-  cd $openssl_source_path
+  library_source_path=$source_path/openssl
+  library_build_path=$build_path/openssl
+  mkdir -p $library_build_path
+  cd $library_source_path
   git checkout OpenSSL_1_1_1d
-  ./Configure darwin64-x86_64-cc enable-ec_nistp_64_gcc_128 no-ssl2 no-ssl3 no-comp --prefix=$openssl_build_path && make && make install_sw
+  ./Configure darwin64-x86_64-cc enable-ec_nistp_64_gcc_128 no-ssl2 no-ssl3 no-comp --prefix=$library_build_path && make && make install_sw
   echo "build openssl end"
 }
 
 build_rapidjson(){
   echo "build rapidjson start"
-  rapidjson_source_path=$source_path/rapidjson
-  rapidjson_build_path=$build_path/rapidjson
-  mkdir -p $rapidjson_build_path
-  cd $rapidjson_source_path
+  library_source_path=$source_path/rapidjson
+  library_build_path=$build_path/rapidjson
+  mkdir -p $library_build_path
+  cd $library_source_path
   git checkout v1.1.0
-  cmake -DCMAKE_INSTALL_PREFIX=$rapidjson_build_path -DRAPIDJSON_BUILD_DOC=OFF -DRAPIDJSON_BUILD_TESTS=OFF -DRAPIDJSON_BUILD_EXAMPLES=OFF $rapidjson_source_path
+  cmake -DCMAKE_INSTALL_PREFIX=$library_build_path -DRAPIDJSON_BUILD_DOC=OFF -DRAPIDJSON_BUILD_TESTS=OFF -DRAPIDJSON_BUILD_EXAMPLES=OFF $library_source_path
   make && make install
   echo "build rapidjson end"
 }
 
 build_easyloggingpp(){
   echo "build easyloggingpp start"
-  easyloggingpp_source_path=$source_path/easyloggingpp
-  easyloggingpp_build_path=$build_path/easyloggingpp
-  mkdir -p $easyloggingpp_build_path
-  cd $easyloggingpp_source_path
+  library_source_path=$source_path/easyloggingpp
+  library_build_path=$build_path/easyloggingpp
+  mkdir -p $library_build_path
+  cd $library_source_path
   git checkout v9.96.7
-  cmake -DCMAKE_INSTALL_PREFIX=$easyloggingpp_build_path -Dbuild_static_lib=ON $easyloggingpp_source_path
+  cp -fr ../../patch/easyloggingpp/CMakeLists.txt ./
+  cmake -DCMAKE_INSTALL_PREFIX=$library_build_path -Dbuild_static_lib=ON -Dbuild_shared_lib=ON $library_source_path
   make && make install
   echo "build easyloggingpp end"
 }
 
 build_jwtcpp(){
   echo "build jwt-cpp start"
-  jwtcpp_source_path=$source_path/jwt-cpp
-  jwtcpp_build_path=$build_path/jwt-cpp
-  cd $jwtcpp_source_path
+  library_source_path=$source_path/jwt-cpp
+  library_build_path=$build_path/jwt-cpp
+  cd $library_source_path
   git checkout v0.3.1
   echo "build jwt-cpp end"
 }
 
-build_boost(){
-  #download *nix platform
-  cd $build_path
-  wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz
-  rm -fr boost
-  tar xzf boost_1_72_0.tar.gz
-  mv boost_1_72_0 boost
+build_libuv(){
+  echo "build libuv start"
+  library_source_path=$source_path/libuv
+  library_build_path=$build_path/libuv
+  mkdir -p $library_build_path
+  cd $library_source_path
+  git checkout v1.40.0
+  cmake -DCMAKE_INSTALL_PREFIX=$library_build_path $library_source_path
+  make && make install
+  echo "build libuv end"
 }
 
-build_sigcplusplus(){
-  echo "configure sigc++ start"
-  sigcplusplus_source_path=$source_path/libsigcplusplus
-  git checkout 3.0.3
-  echo "configure sigc++ end"
+build_uvcpp(){
+  echo "build uvcpp start"
+  library_source_path=$source_path/uv-cpp
+  library_build_path=$build_path/uv-cpp
+  mkdir -p $library_build_path
+  cd $library_source_path
+  git checkout 1.5.4
+  cp -fr ../../patch/uv-cpp/CMakeLists.txt ./
+  cmake -DCMAKE_INSTALL_PREFIX=$library_build_path $library_source_path
+  make && make install
+  echo "build uvcpp end"
 }
 
 main(){
@@ -94,10 +106,8 @@ main(){
   build_rapidjson
   build_easyloggingpp
   build_jwtcpp
-  build_boost
-  build_sigcplusplus
+  build_libuv
+  build_uvcpp
 }
 
 main
-
-
