5c6580a4bfef04168dd57eaf8821036345be7d3f
streamingnology <37998919+streamingnology@users.noreply.github.com>
2021-01-14 01:26:26 -0600

    build pass

---
+++ common/src/core/event.h
@@ -0,0 +1,38 @@
+//==============================================================================
+//
+//  OvenMediaEngine
+//
+//  Created by Hyunjun Jang
+//  Copyright (c) 2018 AirenSoft. All rights reserved.
+//
+//==============================================================================
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+#include "media/rtmp/helper/ovdata_structure.h"
+namespace ov
+{
+	class Event
+	{
+	public:
+		explicit Event(bool manual_reset = false);
+
+		// 이벤트 설정
+		bool SetEvent();
+
+		bool Reset();
+
+		// timeout in milliseconds
+		bool Wait(int timeout = Infinite);
+		bool Wait(std::chrono::time_point<std::chrono::system_clock> time_point);
+
+	protected:
+		bool _manual_reset;
+
+		std::mutex _mutex;
+		std::condition_variable _condition;
+		bool _event;
+	};
+}
+
