dc4eca4c0e1570f944b5e14a6a177387e348e9e1
streamingnology <37998919+streamingnology@users.noreply.github.com>
2021-01-29 17:01:04 -0600

    delete useless class

--- common/src/core/snyutils.h
+++ common/src/core/snyutils.h
@@ -14,12 +14,5 @@ namespace sny {
 namespace SnyUtils {
   SnyUI64 convertTime(SnyUI64 time_value, SnyUI64 from_time_scale, SnyUI64 to_time_scale);
   std::string formatstring(const char* format, ...);
-  std::vector<std::string> splitUri(std::string uri);
-  SnyDataBuffer listDirectoryContent(std::string path);
-  std::map<int, std::vector<std::string>> getDirectoryContent(std::string path);
-  std::vector<std::string> getIpAddress();
-  std::string getCurrentAppDirectory();
-  SnyServiceType getServiceTypeByPath(const std::string path);
-  SnyFileDir isFileorDir(std::string& path);
 }  // namespace SnyUtils
 }  // namespace sny
